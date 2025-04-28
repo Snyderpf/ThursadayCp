a3b0790c3e6520f113c15b7d21985b948c68ef00
snyder <petitsnyder@gmail.com>
2025-04-27 20:47:59 -0400

    reverse string without swap()

---
+++ ReverseString.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int n = s.size();
+        int l = 0, r = n - 1;
+
+        while (l < r)
+        {
+            char temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            r--;
+            l++;
+        }
+    }
+};
